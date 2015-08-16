32246d08446c72e54be7d2a9c6e10269950af2f4
U-DAVE-WIN10\daves <daves125125@gmail.com>
2015-08-16 15:35:02 +0100

    cleanup

--- src/main/java/com/djh/udp/UDPBroadcastLoader.java
+++ src/main/java/com/djh/udp/UDPBroadcastLoader.java
@@ -28,7 +28,7 @@ public class UDPBroadcastLoader {
             clientSocket.send(sendPacket);
 
             System.out.println("SENT PACKET:" + sendPacket);
-            Thread.sleep(3000);
+            Thread.sleep(2500);
         }
     }
 
